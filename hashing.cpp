498ae33ead2f85b1363f8ebb5321370186734dbb
Dennis Kühnlein <d.kuehnlein@outlook.com>
2025-08-11 16:42:09 +0200

    added file-header

--- hashing.cpp
+++ hashing.cpp
@@ -1,3 +1,20 @@
+/********************************************************************
+ * DNS-Tracker
+ *
+ * This tool is build for use at DTAG and Deutsche Telekom Technik.
+ * The purpose of this program is to trigger the DTAG-BPA-DNS-resolver
+ * to monitor changes on external DNS-side.
+ * The goal is to verify the delay of changing the DNS-response at
+ * DTAG-internal systems and made the change available for the customers
+ * on DTAG-external-site
+ *
+ * Purpose of this file:
+ * In the Hasing-namespace are a bunch of helper-function for easy
+ * and reliable comparison between the responses
+ *
+ * Author: Dennis Kuehnlein (2025)
+********************************************************************/
+
 #include "hashing.h"
 
 #include <QCoreApplication>
